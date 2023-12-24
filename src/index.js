49e9cfcd50f0ff97cf3501249c1a069b0df7d631
Hollieb63 <127986870+Hollieb63@users.noreply.github.com>
2023-12-24 15:35:19 +0000

    Changed h1 font and added styling to strong element

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@
    let instructionsInput=document.querySelector("#instructions");
 let apiKey="f176dboa0e40a3602864ef25ecact0b3";
 let prompt=`Please provide a short english poem about ${instructionsInput.value}`;
-let context="You are a literary genius who specialises in english poetry. Your mission is to a four line poem in basic HTML.Do not include the poem title and sign the poem with 'SheCodes AI' in bold at the end. ";
+let context="You are a literary genius who specialises in english poetry. Your mission is to a four line poem in basic HTML.Do not include the poem title and sign the poem with <strong> 'SheCodes AI'</strong> in bold at the end. ";
 let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 axios.get(apiUrl).then(showAnswer);
     }
